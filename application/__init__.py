1f5f957549cf71a0ef1e7d7d071f0525902a8d49
Viannaiv <viva95@windowslive.com>
2018-04-06 23:32:51 +0300

    added postgre database to be used in heroku and modified Procfile

--- application/__init__.py
+++ application/__init__.py
@@ -2,6 +2,13 @@
 app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
+
+import os
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///bookshelf.db"
 app.config["SQLALCHEMY_ECHO"] = True
 
@@ -21,7 +28,7 @@ app.config["SECRET_KEY"] = urandom(32)
 
 from flask_login import LoginManager
 login_manager = LoginManager()
-login_manager.setup_app(app)
+login_manager.init_app(app)
 
 login_manager.login_view = "auth_login"
 login_manager.login_message = "Please login to use this functionality."
@@ -30,4 +37,7 @@ login_manager.login_message = "Please login to use this functionality."
 def load_user(user_id):
     return User.query.get(user_id)
 
-db.create_all()
+try: 
+    db.create_all()
+except:
+    pass
