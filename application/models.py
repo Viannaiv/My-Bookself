e5aeae74f60272c7559ba816b70a55aa4f0e2962
Viannaiv <viva95@windowslive.com>
2018-04-12 20:05:08 +0300

    added abstract class Base

---
+++ application/models.py
@@ -0,0 +1,10 @@
+﻿from application import db
+
+class Base(db.Model):
+    __abstract__ = True
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+        onupdate=db.func.current_timestamp())
