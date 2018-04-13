4192c05fe0170da69bb5bbb44744a17e8925a1b3
Viannaiv <viva95@windowslive.com>
2018-04-13 18:06:01 +0300

    added the possibility of adding new editions to user's bookshelf

--- application/editions/forms.py
+++ application/editions/forms.py
@@ -1,1 +1,10 @@
-﻿
+﻿from flask_wtf import FlaskForm
+from wtform import StringField, IntegerField, BooleanField, validators
+
+class EditionForm(FlaskForm):
+    name = StringField("Name: ", [validators.Length(min=2, max=150)])
+    printed = IntegerField("Printed in: (year) ", [validators.NumberRange(min=1377, max=2018)])
+    publisher = StringField("Publisher: ", [validators.Length(min=1, max=100)])
+    language = StringField("Language: ", [validators.Length(min=3, max=50)])
+    read = BooleanField("I have read this book: ")
+
--- application/editions/views.py
+++ application/editions/views.py
@@ -1,11 +1,32 @@
 ﻿from application import app, db
 from flask import redirect, render_template, request, url_for
 from application.editions.models import Edition
-# form imports here later
-from flask_login import login_required
+from application.editions.forms import EditionForm
+from flask_login import login_required, current_user
 
 @app.route("/editions", methods=["GET"])
 @login_required
 def editions_index():
     return render_template("editions/list.html", editions = Edition.query.all())
 
+@app.route("/editions/new/", methods=["GET"])
+@login_required
+def editions_form():
+    return render_template("editions/new.html", form = EditionForm())
+
+@app.route("/editions/new/", methods=["POST"])
+@login_required
+def editions_create():
+    form = EditionForm(request.form)
+
+    if not form.validate():
+        return render_template("editions/new.html", form = form)
+
+    e = Edition(form.name.data, form.printed.data, form.publisher.data, 
+                    form.language.data, form.read.data)
+    e.account_id = current_user.id
+    
+    db.session().add(e)
+    db.session().commit()
+    
+    return redirect(url_for("editions_index"))
