833ecaea8254c2241bcecea7dbd911c92fc4ba1d
Viannaiv <viva95@windowslive.com>
2018-04-27 15:07:18 +0300

    all methods using login_required now use the one from __init__.py

--- application/editions/views.py
+++ application/editions/views.py
@@ -1,21 +1,21 @@
-﻿from application import app, db
+﻿from application import app, db, login_required
 from flask import redirect, render_template, request, url_for
 from application.editions.models import Edition
 from application.editions.forms import EditionForm, EditionEditName, EditionEditPrinted, EditionEditPublisher, EditionEditLanguage, EditionEditRead
-from flask_login import current_user, login_required, login_manager
+from flask_login import current_user, login_manager
 
 @app.route("/editions", methods=["GET"])
-@login_required
+@login_required()
 def editions_index():
     return render_template("editions/list.html", editions = Edition.query.filter_by(account_id = current_user.id))
 
 @app.route("/editions/new/", methods=["GET"])
-@login_required
+@login_required()
 def editions_form():
     return render_template("editions/new.html", form = EditionForm())
 
 @app.route("/editions/new/", methods=["POST"])
-@login_required
+@login_required()
 def editions_create():
     form = EditionForm(request.form)
 
@@ -34,17 +34,17 @@ def editions_create():
     return redirect(url_for("editions_index"))
 
 @app.route("/editions/<edition_id>/", methods=["GET"])
-@login_required
+@login_required()
 def edition_view(edition_id):
     return render_template("editions/edition.html", edition = Edition.query.get(edition_id))
 
 @app.route("/editions/editname/<edition_id>/", methods=["GET"])
-@login_required
+@login_required()
 def edition_editnameform(edition_id):  
     return render_template("editions/editname.html", form = EditionEditName(), edition_id=edition_id)
 
 @app.route("/editions/editname/<edition_id>/", methods=["POST"])
-@login_required
+@login_required()
 def edition_editname(edition_id):
     form = EditionEditName(request.form)
     
@@ -63,12 +63,12 @@ def edition_editname(edition_id):
     return redirect(url_for("edition_view", edition_id=edition_id))
 
 @app.route("/editions/editprinted/<edition_id>/", methods=["GET"])
-@login_required
+@login_required()
 def edition_editprintedform(edition_id):  
     return render_template("editions/editprinted.html", form = EditionEditPrinted(), edition_id=edition_id)
 
 @app.route("/editions/editprinted/<edition_id>/", methods=["POST"])
-@login_required
+@login_required()
 def edition_editprinted(edition_id):
     form = EditionEditPrinted(request.form)
 
@@ -87,12 +87,12 @@ def edition_editprinted(edition_id):
     return redirect(url_for("edition_view", edition_id=edition_id))
 
 @app.route("/editions/editpublisher/<edition_id>/", methods=["GET"])
-@login_required
+@login_required()
 def edition_editpublisherform(edition_id):  
     return render_template("editions/editpublisher.html", form = EditionEditPublisher(), edition_id=edition_id)
 
 @app.route("/editions/editpublisher/<edition_id>/", methods=["POST"])
-@login_required
+@login_required()
 def edition_editpublisher(edition_id):
     form = EditionEditPublisher(request.form)
 
@@ -111,12 +111,12 @@ def edition_editpublisher(edition_id):
     return redirect(url_for("edition_view", edition_id=edition_id))
 
 @app.route("/editions/editlanguage/<edition_id>/", methods=["GET"])
-@login_required
+@login_required()
 def edition_editlanguageform(edition_id):  
     return render_template("editions/editlanguage.html", form = EditionEditLanguage(), edition_id=edition_id)
 
 @app.route("/editions/editlanguage/<edition_id>/", methods=["POST"])
-@login_required
+@login_required()
 def edition_editlanguage(edition_id):
     form = EditionEditLanguage(request.form)
 
@@ -135,12 +135,12 @@ def edition_editlanguage(edition_id):
     return redirect(url_for("edition_view", edition_id=edition_id))
 
 @app.route("/editions/editread/<edition_id>/", methods=["GET"])
-@login_required
+@login_required()
 def edition_editreadform(edition_id):  
     return render_template("editions/editread.html", form = EditionEditRead(), edition_id=edition_id)
 
 @app.route("/editions/editread/<edition_id>/", methods=["POST"])
-@login_required
+@login_required()
 def edition_editread(edition_id):
     form = EditionEditRead(request.form)
 
