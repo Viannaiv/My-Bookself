06c020b589ca0ea3fab8974512e401ae592c3584
Viannaiv <viva95@windowslive.com>
2018-04-27 18:38:39 +0300

    applied bootstrap to tables, created files for adding Author to app

---
+++ application/authors/models.py
@@ -0,0 +1,9 @@
+﻿from application import db
+from application.models import Base
+
+class Author(Base):
+    editions = db.relationship("Edition", backref='format', lazy=True)
+
+
+    def __init__(self, name):
+        self.name = name
