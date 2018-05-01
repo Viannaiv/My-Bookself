750d89b266db83f1afa6e7b542b8b747f045ab07
Viannaiv <viva95@windowslive.com>
2018-05-01 04:37:13 +0300

    deleted unnecessary line

--- application/authors/models.py
+++ application/authors/models.py
@@ -4,6 +4,5 @@ from application.models import Base
 class Author(Base):
     editions = db.relationship("Edition", backref='format', lazy=True)
 
-
     def __init__(self, name):
         self.name = name
