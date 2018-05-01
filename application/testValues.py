77eb2b10b93b27f7961cdd6b1e59eb4ce96fc446
Viannaiv <viva95@windowslive.com>
2018-05-01 04:32:49 +0300

    fixed a problem

--- application/testValues.py
+++ application/testValues.py
@@ -4,6 +4,7 @@ from application.formats.models import Format
 from application.works.models import Work
 from application.auth.models import Role, User
 
+#delete user later
 def create_test_data():
     role = Role(1, "ADMIN")
     role2 = Role(2, "NORMAL")
@@ -11,14 +12,12 @@ def create_test_data():
     db.session.add(role2)
     db.session.commit()
 
-    #delete later!
     user = User("Vivianna", "Vianna", "plopetiplop")
     user.role_id = 1
     db.session().add(user)
     db.session.commit()
 
     work = Work("No work defined yet", 1700, "Undefined work.")
-    work.id = 1
     work2 = Work("Empress Dowager Cixi", 2013, "The history of a woman chosen at the age of 16 to be one of the emperor's concubines, who eventually becomes the dowager empress of China.")
     work3 = Work("Humanity", 1999, "A Moral History of the Twentieth Century")
     db.session().add(work)
