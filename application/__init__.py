45da0d230ae1655ffec4f5d34f611280e41763ab
Viannaiv <viva95@windowslive.com>
2018-05-10 12:12:26 +0300

    added css, fixed a title

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,5 @@
 ﻿from flask import Flask, flash, redirect
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/static")
 
 from flask_sqlalchemy import SQLAlchemy
 
@@ -20,7 +20,7 @@ app.config["SECRET_KEY"] = urandom(32)
 
 from flask_login import LoginManager, current_user
 login_manager = LoginManager()
-login_manager.setup_app(app) #Here was .init_app
+login_manager.setup_app(app)
 
 login_manager.login_view = "auth_login"
 login_manager.login_message = "Please login to use this functionality."
